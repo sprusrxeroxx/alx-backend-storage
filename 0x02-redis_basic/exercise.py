b0a7d73b5cc06461ab3422be41e7c1406c05692e
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2024-08-14 15:29:00 +0200

    chore: created main method for Cache class

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from uuid import uuid4
+import redis
+
+class Cache:
+    """A simple Cache class that takes in a redis instance and flushes it"""
+    def __init__(self):
+        self._redis = redis.Redis(host='localhost', port=6379, db=0)
+        self._redis.flushdb()
+        
+    def store(self, data):
+        """A method that takes in a data arguement, assigns a randomly generated key and returns that key"""
+        self.key = str(uuid4())
+        self.set(key, data)
+
+        return (key)
