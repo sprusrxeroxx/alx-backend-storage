5a447754562c5b58747bef176a1b8f540e603a16
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2024-08-15 10:19:04 +0200

    chore: set parameter types

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -10,7 +10,7 @@ class Cache:
         self._redis = redis.Redis(host='localhost', port=6379, db=0)
         self._redis.flushdb()
         
-    def store(self, data:Union[str,float,int,bytes])->str:
+    def store(self, data:Union[str, bytes, int, float])->str:
         """A method that takes in a data arguement, assigns a randomly generated key and returns that key"""
         key = str(uuid4())
         self._redis.set(key, data)
